3f715035989e6a78768a02e71d01f5af974276c0
mrludovicc <nadal46@hotmail.com>
2023-04-14 16:48:03 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/styles.css';
+import React, { useState, useEffect } from 'react';
+import Recipe from './components/Recipe';
 
 function App() {
+  const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('/api/recipe')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setRecipe(data.recipe);
+        setLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <Recipe recipe={recipe} />
+      )}
     </div>
   );
 }
---
+++ src/components/Recipe.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Recipe({ recipe }) {
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>{recipe.name}</h1>
+      </header>
+      <main>
+        <section>
+          <h2>Ingredients:</h2>
+          <ul>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </section>
+        <section>
+          <h2>Instructions:</h2>
+          <ol>
+            {recipe.instructions.map((instruction, index) => (
+              <li key={index}>{instruction}</li>
+            ))}
+          </ol>
+        </section>
+        <section>
+          <h2>Nutrition Information:</h2>
+          <p>Calories per serving: {recipe.calories}</p>
+        </section>
+        <section>
+          <h2>Image:</h2>
+          <img src={recipe.image} alt={recipe.name} />
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Recipe;
