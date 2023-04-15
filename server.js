7e09ffc557a27953e927c2798eeed0c4b5c137ae
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-15 20:07:45 -0300

    Small changes to the port.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const axios = require("axios");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const path = require('path');
 app.use(morgan('tiny'));
 app.use(cors());
