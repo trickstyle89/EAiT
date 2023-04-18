5bb5bb6058307bd1577e091cffb365a09f765bb7
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-04-17 23:59:11 -0400

    Fix main.js file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,49 +1,50 @@
-import { Routes, Route, Link } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import Recipe from "./Recipe";
-import DiscreteSlider from "./TimeSlider";
+import { Routes, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import Recipe from './Recipe';
 
 const Main = () => {
   const [recipe, setRecipe] = useState(null);
 
   useEffect(() => {
+    const abortController = new AbortController();
+
     async function fetchData() {
       try {
-        const response = await fetch("/api/recipe");
+        const response = await fetch('/api/recipe', { signal: abortController.signal });
         const data = await response.json();
         setRecipe(data);
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
     }
+
     fetchData();
+
+    return () => {
+      abortController.abort();
+    };
   }, []);
+
 
   return (
     <Routes>
-      <Route
-        exact
-        path="/"
-        element={
-          <Link to="/recipes">
-            <button variant="outlined">Recipes</button>
-          </Link>
-        }
-      />
-      <Route exact path="/ingredients" element={<h1>hello</h1>} />
-      <Route
-        exact
-        path="/recipes"
-        element={
-          <>{recipe ? <Recipe recipe={recipe} /> : <div>Loading...</div>}</>
-        }
-      />
-      <Route
-        exact
-        path="/preferences"
-        element={<DiscreteSlider></DiscreteSlider>}
-      />
+      <Route exact path="/" element={
+        <Link to="/recipes">
+          <button variant="outlined">
+            Recipes
+          </button>
+        </Link>
+      } />
+      <Route exact path="/ingredients" element={
+        <h1>hello</h1>
+      } />
+      <Route exact path="/recipes" element={
+        <>
+          {recipe ? <Recipe recipe={recipe} /> : <div>Loading...</div>}
+        </>
+      } />
     </Routes>
-  );
+
+  )
 };
 export default Main;
